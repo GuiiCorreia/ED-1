1a0958d3d29f208006be9930d30ab8269cf87e76
Guilherme Correia <78569072+GuiiCorreia@users.noreply.github.com>
2022-04-04 23:36:39 -0300

    Add files via upload

---
+++ atv1/atvPortugol.java
@@ -0,0 +1,23 @@
+public class atvPortugol {
+        public static void main(String[] args) {
+
+                System.out.println("raiz: ,raiz(64)\n");
+                System.out.println("raiz com exp e log e realint: , rea1int(exp(1/2*1og(64)))\n");
+                System.out.println("raiz com exp e log sem realint: h',exp(1/2*log(64))\n");
+                System.out.println("formatar(sen(45*pi/180)+0.0001,3))\n");
+                System.out.println("potencia com exp e log e formatar: ,formatar(exp(3*log(8))+0.001,3)\n");
+                System.out.println("potencia com exp e log sem formatar: ,exp(3*1og(8))\n");
+                System.out.println("potencia com operador ** e formatar: uu,formatar(8**3,3)\n");
+                System.out.println("raiz cubica: ,exp(1/3*log(8))\n");
+                System.out.println("absoluto: ,abs(-8)\n");
+                System.out.println("absoluto: ,abs(8)\n");
+                System.out.println("convertendo para inteiro 5.5: ,realint(5.5)\n");
+                System.out.println("convertendo para inteiro 6.5: ,realint(6.5)\n");
+                System.out.println("convertendo para inteiro 6.5 + 0.0001: ,realint(6.5+0.0001)\n");
+                System.out.println("convertendo para inteiro 5.45: ,realint(5.45)\n");
+                System.out.println("convertendo para inteiro 5.51: ,realint(5.51)\n");
+                System.out.println("convertendo para real 87: ,intreal(87)\n");
+                System.out.println("convertendo para int 3/4: ,realint(314)\n");
+
+        }
+}
